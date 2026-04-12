e62243dbbf87941e4e5aa2ba8870a559c8c651df
Nabil Lambattan <lambattannabil2000@gmail.com>
2026-04-12 20:00:30 +0100

    Save data and generate HTML  for delivery options

---
+++ data/deliveryOptions.js
@@ -0,0 +1,17 @@
+export const deliveryOptions = [
+  {
+    id: '1',
+    deliveryDays: 7,
+    princeCents: 0
+  },
+  {
+    id: '2',
+    deliveryDays: 3,
+    princeCents: 499
+  },
+  {
+    id: '3',
+    deliveryDays: 1,
+    princeCents: 999
+  }
+];
