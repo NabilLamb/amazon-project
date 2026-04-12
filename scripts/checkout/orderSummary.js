c8dfccae57c0ede31e38136a79141311233b11a4
Nabil Lambattan <lambattannabil2000@gmail.com>
2026-04-12 21:02:12 +0100

    Save data and generate HTML for payment summary

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -1,27 +1,16 @@
 import { cart, deleteFromCart, updateDeliveryOption } from '../../data/cart.js';
-import { products } from '../../data/products.js';
+import { products, getProduct } from '../../data/products.js';
 import { formatCurrency } from '../utils/money.js';
-import { deliveryOptions } from '../../data/deliveryOptions.js';
+import { deliveryOptions, getDeliveryOption } from '../../data/deliveryOptions.js';
 import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
 
 export function renderOrderSummary() {
   let cartSummaryHTML = '';
   cart.forEach((cartItem) => {
     const productId = cartItem.productId;
-    let matchingProduct;
-    products.forEach(product => {
-      if (product.id === productId) {
-        matchingProduct = product;
-      }
-    });
-
+    const matchingProduct = getProduct(productId);
     const deliveryOptionId = cartItem.deliveryOptionId;
-    let matchingDeliveryOption;
-    deliveryOptions.forEach(deliveryOption => {
-      if (deliveryOption.id === deliveryOptionId) {
-        matchingDeliveryOption = deliveryOption;
-      }
-    });
+    const matchingDeliveryOption = getDeliveryOption(deliveryOptionId);
 
     const today = dayjs();
     const deliveryDate = today.add(matchingDeliveryOption.deliveryDays, 'days').format('dddd, MMMM D');
