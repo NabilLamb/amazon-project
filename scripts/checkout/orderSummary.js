db50f4aaf319226fe0e1ed82cc3ae04cf971e375
Nabil Lambattan <lambattannabil2000@gmail.com>
2026-04-16 22:02:26 +0100

    Create automated test for orderSummary

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -18,7 +18,7 @@ export function renderOrderSummary() {
     const deliveryDate = today.add(matchingDeliveryOption.deliveryDays, 'days').format('dddd, MMMM D');
 
     cartSummaryHTML += `
-  <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
+  <div class="cart-item-container js-cart-item-container js-cart-item-container-${matchingProduct.id}">
     <div class="delivery-date">
       Delivery date: ${deliveryDate}
     </div>
@@ -34,7 +34,7 @@ export function renderOrderSummary() {
         <div class="product-price">
           $${formatCurrency(matchingProduct.priceCents)}
         </div>
-        <div class="product-quantity">
+        <div class="product-quantity js-product-quantity-${matchingProduct.id}">
           <span>
             Quantity: <span class="quantity-label">${cartItem.quantity}</span>
           </span>
@@ -45,7 +45,7 @@ export function renderOrderSummary() {
           <span class="save-quantity-link link-primary js-save-link" data-product-id="${matchingProduct.id}">
             Save
           </span>
-          <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
+          <span class="delete-quantity-link link-primary js-delete-link js-delete-link-${matchingProduct.id}" data-product-id="${matchingProduct.id}">
             Delete
           </span>
         </div>
